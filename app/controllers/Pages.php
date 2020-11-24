eaa065c8f936cdf7a9bd27b16bacce21fd9bcc3d
Youness <elkhatabi.youness@gmail.com>
2020-11-24 14:28:46 +0100

    set inheritance

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,12 +1,12 @@
 <?php
-    class Pages {
+    class Pages extends Controller {
         public function __construct()
         {
 
         }
 
         public function index(){
-            
+    
         }
 
         public function about($id){
