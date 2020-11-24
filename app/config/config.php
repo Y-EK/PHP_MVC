69ad9adb7a62f044df9cda660c06035a05c9f9f9
Youness <elkhatabi.youness@gmail.com>
2020-11-24 15:32:23 +0100

    define constants

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php 
+    // App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // URL Root
+    define('URLROOT', 'http://localhost/PHP_MVC');
+    // Site Name
+    define('SITENAME', 'TraversyMVC');
