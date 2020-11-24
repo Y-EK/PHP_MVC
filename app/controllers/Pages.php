3c0d417b3484691c7b07683de5a75d0532a52ef4
Youness <elkhatabi.youness@gmail.com>
2020-11-24 15:35:51 +0100

    edit and test the function index

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -6,10 +6,13 @@
         }
 
         public function index(){
-    
+            $data = [
+                'title'=>'Welcome'
+            ];
+            $this->view('pages/index', $data);
         }
 
-        public function about($id){
-            echo $id;
+        public function about(){
+            $this->view('pages/about');
         }
     }
