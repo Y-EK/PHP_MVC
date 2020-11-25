a635e802b850f8ccbb53bef44f76826760c0fc12
Youness <elkhatabi.youness@gmail.com>
2020-11-25 15:25:10 +0100

    typo

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,5 +1,5 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <h1><?php echo $data['title']; ?></h1>
-<p>This is the TraversyMVC frameword.</p>
+<p>This is the TraversyMVC framework.</p>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
 
