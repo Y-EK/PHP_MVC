0600669fc43a9f51e24fea07097cd4f647ae2444
Youness <elkhatabi.youness@gmail.com>
2020-11-25 14:17:12 +0100

    add query result

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -6,9 +6,13 @@
         }
 
         public function index(){
+            $posts = $this->postModel->getPosts();
+
             $data = [
-                'title'=>'Welcome'
+                'title'=>'Welcome',
+                'posts' => $posts
             ];
+            
             $this->view('pages/index', $data);
         }
 
